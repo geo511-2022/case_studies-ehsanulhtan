97ef7a6c7825c0ab9ea3205bd7e43677687947ee
ehsanulhtan <ehsanulh@buffalo.edu>
2022-10-09 13:25:20 -0400

    CS_01_Submission

--- week_01/case_study_01.R
+++ week_01/case_study_01.R
@@ -1,2 +1,9 @@
-data(iris)
+#load the iris data set
+data("iris")
+
+#Calculate mean of the Petal.length field 
+petal_lenth_mean <- mean(iris$Petal.Length)
+
+#Plot the distribution of the Petal.length
+hist(iris$Petal.Length,col = "red", main = "Histogram of Petal Length", xlab = "Petal Length" , ylab = "Frequency" , xlim = c(1,7), ylim = c(0,50))
 
